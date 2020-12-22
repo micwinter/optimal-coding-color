984b4e3ded4c381dc426c5bc210b41cf9242f310
micwinter <micxwinter@gmail.com>
2020-12-22 09:44:14 -0800

    Latest version of pca and model evaluation

--- autoencoder.py
+++ autoencoder.py
@@ -20,70 +20,76 @@ from encoders import LinearAutoencoder, ConvAutoencoder, LinearAESpectrum, Linea
 
 # constants for data loading
 IM_PATH = '/media/big_hdd/opt-color/hyperspec_ims'  # YOUR DATA PATH HERE
-BATCH_SIZE = 128
+BATCH_SIZE = 64
 NUM_PATCHES = 100
 # grid_PATCH_SIZE = [16]#[16, 60, 100]
 # constants for autoencoder
-NUM_EPOCHS = 10000
+NUM_EPOCHS = 1000
 # NUM_NEURONS = 100
 NUM_CONES = 3
-IN_NOISE_SIGMA = 0.4
-OUT_NOISE_SIGMA = 2
-# grid_LEARNING_RATE = [1e-4]#[1e1, 1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5]
-
-ext = 'linaespec2_noise'
-
 PATCH_SIZE = 16
 LEARNING_RATE = 1e-3
 
-now = datetime.datetime.now() # current timestamp
-savename = f'{ext}_{now}_checkpoint_grid_ps_{PATCH_SIZE}_lr_{LEARNING_RATE}.t7'
-
-
-# for PATCH_SIZE in grid_PATCH_SIZE:
-#     for LEARNING_RATE in grid_LEARNING_RATE:
-
-print(f'Patch size: {PATCH_SIZE}, Learning rate: {LEARNING_RATE}')
-
-# trainloader, testloader = data_initializer(patch_size=PATCH_SIZE)
 trainloader, testloader, num_channels = data_initializer(patch_size=PATCH_SIZE, num_patches=NUM_PATCHES)
-# get the computation device
-device = get_device()
-print(f'device: {device}')
 
-# train the network
-# net = LinearAutoencoder(patch_size=PATCH_SIZE, num_neurons=NUM_NEURONS)
-# net = ConvAutoencoder(in_channels=num_channels)
-net = LinearAESpectrum2(
-        in_channels=num_channels,
-        patch_size=PATCH_SIZE,
-        num_cones=NUM_CONES,
-        in_noise=IN_NOISE_SIGMA,
-        out_noise=OUT_NOISE_SIGMA
-        )
-criterion = nn.MSELoss()
-optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE)
-# load the neural network onto the device
-net.to(device)
-train_loss, test_loss = train_autoencoder(
-                net,
-                trainloader,
-                testloader,
-                PATCH_SIZE,
-                NUM_EPOCHS,
-                LEARNING_RATE,
-                optimizer,
-                device,
-                criterion,
-                savename)
+for in_noise in [0.01, 0.1, 0.5, 1, 2]:
+    for out_noise in [0.01, 0.1, 0.5, 1, 2]:
+# IN_NOISE_SIGMA = 0.4
+# OUT_NOISE_SIGMA = 2
+# grid_LEARNING_RATE = [1e-4]#[1e1, 1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5]
 
-# Plot of train loss
-plt.figure()
-plt.plot(train_loss)
-plt.plot(test_loss)
-plt.legend(['Train Loss', 'Test Loss'])
-plt.title(f'Train/Test Loss | PS {PATCH_SIZE} | LR {LEARNING_RATE}')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.savefig(f'{ext}_{now}_autoencoder_loss_ps_{PATCH_SIZE}_lr_{LEARNING_RATE}.png')
-plt.close()
+        ext = f'linaespec2_noise_innoise_{in_noise}_outnoise_{out_noise}'
+
+        now = datetime.datetime.now() # current timestamp
+        # savename = f'{ext}_{now}_checkpoint_grid_ps_{PATCH_SIZE}_lr_{LEARNING_RATE}.t7'
+        savename = f'{ext}_checkpoint_grid_ps_{PATCH_SIZE}_lr_{LEARNING_RATE}.t7'
+
+
+        # for PATCH_SIZE in grid_PATCH_SIZE:
+        #     for LEARNING_RATE in grid_LEARNING_RATE:
+
+        print(f'Patch size: {PATCH_SIZE}, Learning rate: {LEARNING_RATE}')
+
+        # trainloader, testloader = data_initializer(patch_size=PATCH_SIZE)
+        # trainloader, testloader, num_channels = data_initializer(patch_size=PATCH_SIZE, num_patches=NUM_PATCHES)
+        # get the computation device
+        device = get_device()
+        print(f'device: {device}')
+
+        # train the network
+        # net = LinearAutoencoder(patch_size=PATCH_SIZE, num_neurons=NUM_NEURONS)
+        # net = ConvAutoencoder(in_channels=num_channels)
+        net = LinearAESpectrum2(
+                in_channels=num_channels,
+                patch_size=PATCH_SIZE,
+                num_cones=NUM_CONES,
+                in_noise=in_noise,
+                out_noise=out_noise
+                )
+        criterion = nn.MSELoss()
+        optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE)
+        # load the neural network onto the device
+        net.to(device)
+        train_loss, test_loss = train_autoencoder(
+                        net,
+                        trainloader,
+                        testloader,
+                        PATCH_SIZE,
+                        NUM_EPOCHS,
+                        LEARNING_RATE,
+                        optimizer,
+                        device,
+                        criterion,
+                        savename)
+
+        # Plot of train loss
+        plt.figure()
+        plt.plot(train_loss)
+        plt.plot(test_loss)
+        plt.legend(['Train Loss', 'Test Loss'])
+        plt.title(f'Train/Test Loss | PS {PATCH_SIZE} | LR {LEARNING_RATE}')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        # plt.savefig(f'{ext}_{now}_autoencoder_loss_ps_{PATCH_SIZE}_lr_{LEARNING_RATE}.png')
+        plt.savefig(f'{ext}_autoencoder_loss_ps_{PATCH_SIZE}_lr_{LEARNING_RATE}.png')
+        plt.close()
--- evaluate_model.py
+++ evaluate_model.py
@@ -25,12 +25,15 @@ NUM_CONES = 3
 IN_NOISE_SIGMA = 0.4
 OUT_NOISE_SIGMA = 2
 
-ext = 'linaespec2_noise'
+# ext = 'linaespec2_noise'
+# ext = 'linaespec2_noise_innoise_0.5_outnoise_0.5'
+ext = 'linaespec2_noise_innoise_0.1_outnoise_0.01'
 
 PATCH_SIZE = 16
 LEARNING_RATE = 1e-3
 
-now = '2020-12-21 22:56:39.334282'
+# now = '2020-12-21 22:56:39.334282'
+# now = '2020-12-21 23:14:00.882295'
 
 net = LinearAESpectrum2(
         in_channels=31,
@@ -40,7 +43,8 @@ net = LinearAESpectrum2(
         out_noise=OUT_NOISE_SIGMA
         )
 
-savename = f'{ext}_{now}_checkpoint_grid_ps_{PATCH_SIZE}_lr_{LEARNING_RATE}.t7'
+# savename = f'{ext}_{now}_checkpoint_grid_ps_{PATCH_SIZE}_lr_{LEARNING_RATE}.t7'
+savename = f'{ext}_checkpoint_grid_ps_{PATCH_SIZE}_lr_{LEARNING_RATE}.t7'
 checkpoint = torch.load(savename)
 net.load_state_dict(checkpoint['state_dict'])
 
@@ -53,7 +57,9 @@ plt.legend(['Cone 1', 'Cone 2', 'Cone 3'])
 plt.xticks(np.arange(31), np.arange(400, 710, 10))
 plt.ylabel('Learned Weights')
 plt.xlabel('Wavelength')
-plt.savefig(f'{ext}_{now}_weights.png')
+plt.title('Learned Weights of Autoencoder Latent Space')
+# plt.savefig(f'{ext}_{now}_weights.png')
+plt.savefig(f'{ext}_weights.png')
 
 
 
